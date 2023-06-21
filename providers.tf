8e15243898f2db1728b29ada108797cb4f911821
Jorge Eduardo Zapata Martinez <jzapatamarti@microsoft.com>
2023-06-20 22:23:47 -0400

    modificacion archivo provider

--- providers.tf
+++ providers.tf
@@ -1,15 +1,13 @@
-terraform {  required_providers {
+terraform {
+  required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.61.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
+      version = "=3.0.0"
     }
   }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
